39e96ec0706ff34cac094a7712c82fa0c810cf80
JINWOO <njw9108@naver.com>
2023-02-21 23:59:11 +0900

    feat: address model 생성

---
+++ lib/restaurant/model/address_model.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'address_model.g.dart';
+
+@JsonSerializable()
+class AddressModel {
+  @JsonKey(name: 'address_name')
+  final String address;
+  @JsonKey(name: 'road_address_name')
+  final String roadAddressName;
+  @JsonKey(name: 'place_url')
+  final String url;
+
+  AddressModel({
+    required this.address,
+    required this.roadAddressName,
+    required this.url,
+  });
+
+  factory AddressModel.fromJson(Map<String, dynamic> json) =>
+      _$AddressModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AddressModelToJson(this);
+}
---
+++ lib/restaurant/model/address_model.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'address_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AddressModel _$AddressModelFromJson(Map<String, dynamic> json) => AddressModel(
+      address: json['address_name'] as String,
+      roadAddressName: json['road_address_name'] as String,
+      url: json['place_url'] as String,
+    );
+
+Map<String, dynamic> _$AddressModelToJson(AddressModel instance) =>
+    <String, dynamic>{
+      'address_name': instance.address,
+      'road_address_name': instance.roadAddressName,
+      'place_url': instance.url,
+    };
