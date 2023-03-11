74cd581d3f7a6ed9a8a17e02347c04777c78abc3
JINWOO <njw9108@naver.com>
2023-03-11 15:40:39 +0900

    feat: firebase crashlytics 추가

--- lib/home/view/home_screen.dart
+++ lib/home/view/home_screen.dart
@@ -48,4 +48,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -29,6 +31,15 @@ void main() async {
     name: 'restaurant_recommendation',
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   await dotenv.load();
   String appkey = dotenv.env[nativeAppKey] ?? '';
